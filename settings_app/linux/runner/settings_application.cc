2d104f16f0ea3de9af8dae316d5030f9081efcbf
Tikily <tikily7047@outlook.com>
2026-09-18 18:43:07 +0800

    Fix: fall back to G_APPLICATION_FLAGS_NONE for GLib < 2.74 in settings runner

--- settings_app/linux/runner/settings_application.cc
+++ settings_app/linux/runner/settings_application.cc
@@ -6,6 +6,10 @@
 
 #include "flutter/generated_plugin_registrant.h"
 
+#ifndef G_APPLICATION_DEFAULT_FLAGS
+#define G_APPLICATION_DEFAULT_FLAGS G_APPLICATION_FLAGS_NONE
+#endif
+
 struct _SettingsApplication {
   GtkApplication parent_instance;
   char** dart_entrypoint_arguments;
